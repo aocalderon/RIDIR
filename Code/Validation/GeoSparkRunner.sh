74eaa9550c7ec783819db33945f8afe0c6e987f1
Andres Calderon <acald013@ucr.edu>
2019-04-24 00:18:52 -0700

    Adding benchmark results...

--- Code/Validation/GeoSparkRunner.sh
+++ Code/Validation/GeoSparkRunner.sh
@@ -5,7 +5,8 @@ for n in `seq 1 $N`
 do
     for sample in `seq 0 9`
     do
-	echo "spark-submit /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/Validation/SampleCA_source${sample}.wkt --target ~/Datasets/Validation/SampleCA_target${sample}.wkt"
-	spark-submit /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/Validation/SampleCA_source${sample}.wkt --target ~/Datasets/Validation/SampleCA_target${sample}.wkt
+	tag=$((sample + 1))
+	echo "spark-submit /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/Validation/SampleCA_source${sample}.wkt --target ~/Datasets/Validation/SampleCA_target${sample}.wkt --tag ${tag}0%"
+	spark-submit /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/Validation/SampleCA_source${sample}.wkt --target ~/Datasets/Validation/SampleCA_target${sample}.wkt --tag ${tag}0%
     done
 done
