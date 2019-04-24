2efe7060ad496992ab8fa73071a149570f570a7f
Andres Calderon <acald013@ucr.edu>
2019-04-24 07:41:01 -0700

    Adding more results to geospark full benchmarck...

---
+++ Code/Validation/GeoSparkFull.sh
@@ -0,0 +1,23 @@
+spark-submit --class ArealTester /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/gdf_2000/ --target ~/Datasets/gdf_1990/ --host 169.235.27.138 --partitions 1024 --sample 0.2
+spark-submit --class ArealTester /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/gdf_2000/ --target ~/Datasets/gdf_1990/ --host 169.235.27.138 --partitions 1024 --sample 0.4
+spark-submit --class ArealTester /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/gdf_2000/ --target ~/Datasets/gdf_1990/ --host 169.235.27.138 --partitions 1024 --sample 0.6
+spark-submit --class ArealTester /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/gdf_2000/ --target ~/Datasets/gdf_1990/ --host 169.235.27.138 --partitions 1024 --sample 0.8
+spark-submit --class ArealTester /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/gdf_2000/ --target ~/Datasets/gdf_1990/ --host 169.235.27.138 --partitions 1024 --sample 1.0
+
+spark-submit --class ArealTester /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/gdf_2000/ --target ~/Datasets/gdf_1990/ --host 169.235.27.138 --partitions 1024 --sample 0.2
+spark-submit --class ArealTester /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/gdf_2000/ --target ~/Datasets/gdf_1990/ --host 169.235.27.138 --partitions 1024 --sample 0.4
+spark-submit --class ArealTester /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/gdf_2000/ --target ~/Datasets/gdf_1990/ --host 169.235.27.138 --partitions 1024 --sample 0.6
+spark-submit --class ArealTester /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/gdf_2000/ --target ~/Datasets/gdf_1990/ --host 169.235.27.138 --partitions 1024 --sample 0.8
+spark-submit --class ArealTester /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/gdf_2000/ --target ~/Datasets/gdf_1990/ --host 169.235.27.138 --partitions 1024 --sample 1.0
+
+spark-submit --class ArealTester /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/gdf_2000/ --target ~/Datasets/gdf_1990/ --host 169.235.27.138 --partitions 1024 --sample 0.2
+spark-submit --class ArealTester /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/gdf_2000/ --target ~/Datasets/gdf_1990/ --host 169.235.27.138 --partitions 1024 --sample 0.4
+spark-submit --class ArealTester /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/gdf_2000/ --target ~/Datasets/gdf_1990/ --host 169.235.27.138 --partitions 1024 --sample 0.6
+spark-submit --class ArealTester /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/gdf_2000/ --target ~/Datasets/gdf_1990/ --host 169.235.27.138 --partitions 1024 --sample 0.8
+spark-submit --class ArealTester /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/gdf_2000/ --target ~/Datasets/gdf_1990/ --host 169.235.27.138 --partitions 1024 --sample 1.0
+
+spark-submit --class ArealTester /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/gdf_2000/ --target ~/Datasets/gdf_1990/ --host 169.235.27.138 --partitions 1024 --sample 0.2
+spark-submit --class ArealTester /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/gdf_2000/ --target ~/Datasets/gdf_1990/ --host 169.235.27.138 --partitions 1024 --sample 0.4
+spark-submit --class ArealTester /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/gdf_2000/ --target ~/Datasets/gdf_1990/ --host 169.235.27.138 --partitions 1024 --sample 0.6
+spark-submit --class ArealTester /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/gdf_2000/ --target ~/Datasets/gdf_1990/ --host 169.235.27.138 --partitions 1024 --sample 0.8
+spark-submit --class ArealTester /home/acald013/RIDIR/Code/Areal/target/scala-2.11/areal_2.11-0.1.jar --source ~/Datasets/gdf_2000/ --target ~/Datasets/gdf_1990/ --host 169.235.27.138 --partitions 1024 --sample 1.0
