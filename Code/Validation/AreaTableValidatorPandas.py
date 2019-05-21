b3c606448e546b7a875dfb5aa7e899ad23be4abe
Andres Calderon <acald013@ucr.edu>
2019-05-21 11:33:54 -0700

    Running validation for phili dataset...

--- Code/Validation/AreaTableValidatorPandas.py
+++ Code/Validation/AreaTableValidatorPandas.py
@@ -16,17 +16,17 @@ parser.add_argument("--target", "-t", help="Target polygon file...")
 parser.add_argument("--output", "-o", default="/tmp/geopandas_area_table.tsv", help="Output file...")
 args = parser.parse_args()
 
-data = pd.read_csv(args.source, sep = '\t', header = None, names = ["geometry", "id", "extensive", "intensive"])
-data['geometry'] = data['geometry'].apply(wkt.loads)
-source = gpd.GeoDataFrame(data, geometry='geometry')
+data = pd.read_csv(args.source, sep = '\t', header = None, names = ["geom", "id", "ext", "int"])
+data['geom'] = data['geom'].apply(wkt.loads)
+source = gpd.GeoDataFrame(data, geometry='geom')
 print(source.head())
-print("Source: {}".format(source.shape))
+print("Source: {}".format(source.shape()))
 
-data = pd.read_csv(args.target, sep = '\t', header = None, names = ["geometry", "id"])
-data['geometry'] = data['geometry'].apply(wkt.loads)
-target = gpd.GeoDataFrame(data, geometry='geometry')
+data = pd.read_csv(args.target, sep = '\t', header = None, names = ["geom", "id"])
+data['geom'] = data['geom'].apply(wkt.loads)
+target = gpd.GeoDataFrame(data, geometry='geom')
 print(target.head())
-print("Target: {}".format(target.shape))
+print("Target: {}".format(target.shape()))
 
 (SU, UT) = area_tables(source, target)
 
@@ -37,6 +37,6 @@ for i in range(tobler.shape[0]):
     for j in range(tobler.shape[1]):
         area = tobler[i,j]
         if area > 0:
-            toblerFile.write("{}\t{}\t{}\n".format(source['id'][i], target['id'][j], area))
+            toblerFile.write("{}\t{}\t{}\n".format(source['id'][j], target['id'][i], area))
 toblerFile.close()
 print("Done!")
