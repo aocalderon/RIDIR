9ae15ab4b9fd8a1fc066c4ca1975effade7765d1
aocalderon <andress.calderon@gmail.com>
2019-05-22 11:03:11 -0700

    Sync from dblab...

--- Code/Validation/AreaInterpolateValidatorPandas.py
+++ Code/Validation/AreaInterpolateValidatorPandas.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+#%%
 from shapely.geometry import Polygon
 from shapely import wkt
 import numpy as np
@@ -8,37 +9,24 @@ import pandas as pd
 import geopandas as gpd
 from tobler import area_tables
 import sys
-import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--source", "-s", help="Source polygon file...")
-parser.add_argument("--target", "-t", help="Target polygon file...")
-parser.add_argument("--output", "-o", default="/tmp/geopandas_area_table.tsv", help="Output file...")
-args = parser.parse_args()
-
-data = pd.read_csv(args.source, sep = '\t', header = None, names = ["geom", "id", "ext", "int"])
+#%%
+sourceFile = "/home/and/RIDIR/Datasets/phili2000a.wkt"
+data = pd.read_csv(sourceFile, sep = '\t', header = None, names = ["geom", "id", "ext", "int"])
 data['geom'] = data['geom'].apply(wkt.loads)
 source = gpd.GeoDataFrame(data, geometry='geom')
 print(source.head())
 print("Source: {}".format(source.shape()))
 
-data = pd.read_csv(args.target, sep = '\t', header = None, names = ["geom", "id"])
+targetFile = "/home/and/RIDIR/Datasets/phili1990.wkt"
+data = pd.read_csv(targetFile, sep = '\t', header = None, names = ["geom", "id"])
 data['geom'] = data['geom'].apply(wkt.loads)
 target = gpd.GeoDataFrame(data, geometry='geom')
 print(target.head())
 print("Target: {}".format(target.shape()))
 
+#%%
 (SU, UT) = area_tables(source, target)
-
-tobler = np.dot(SU, UT)
-
-toblerFile = open(args.output, "w")
-for i in range(tobler.shape[0]):
-    for j in range(tobler.shape[1]):
-        area = tobler[i,j]
-        if area > 0:
-            toblerFile.write("{}\t{}\t{}\n".format(source['id'][j], target['id'][i], area))
-toblerFile.close()
 
 intensive_variable = ["int"]
 ST = np.dot(SU, UT)
