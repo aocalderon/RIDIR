d06cfd6e65ec791829678873c138d4e7d8972d50
aocalderon <andress.calderon@gmail.com>
2019-05-22 11:45:21 -0700

    Sync from dblab...

--- Code/Validation/AreaInterpolateValidatorPandas.py
+++ Code/Validation/AreaInterpolateValidatorPandas.py
@@ -11,19 +11,19 @@ from tobler import area_tables
 import sys
 
 #%%
-sourceFile = "/home/and/RIDIR/Datasets/phili2000a.wkt"
+sourceFile = "/home/and/RIDIR/Datasets/phili_2000a.wkt"
 data = pd.read_csv(sourceFile, sep = '\t', header = None, names = ["geom", "id", "ext", "int"])
 data['geom'] = data['geom'].apply(wkt.loads)
 source = gpd.GeoDataFrame(data, geometry='geom')
 print(source.head())
-print("Source: {}".format(source.shape()))
+print("Source: {}".format(source.shape))
 
-targetFile = "/home/and/RIDIR/Datasets/phili1990.wkt"
+targetFile = "/home/and/RIDIR/Datasets/phili_1990.wkt"
 data = pd.read_csv(targetFile, sep = '\t', header = None, names = ["geom", "id"])
 data['geom'] = data['geom'].apply(wkt.loads)
 target = gpd.GeoDataFrame(data, geometry='geom')
 print(target.head())
-print("Target: {}".format(target.shape()))
+print("Target: {}".format(target.shape))
 
 #%%
 (SU, UT) = area_tables(source, target)
@@ -35,10 +35,23 @@ den = np.diag(1./ (area + (area == 0)))
 weights = np.dot(ST, den)
 intensive = []
 for variable in intensive_variables:
-    vals = _nan_check(source_df, variable)
+    vals = _nan_check(source, variable)
     vals.shape = (len(vals), 1)
     est = (vals * weights).sum(axis=0)
     intensive.append(est)
 intensive = np.array(intensive)
 
 print("Done!")
+
+
+#%%
+def _nan_check(df, column):
+    """Check if variable has nan values.
+    Warn and replace nan with 0.0.
+    """
+    values = df[column].values
+    if np.any(np.isnan(values)):
+        wherenan = np.isnan(values)
+        values[wherenan] = 0.0
+        print('nan values in variable: {var}, replacing with 0.0'.format(var=column))
+    return values
