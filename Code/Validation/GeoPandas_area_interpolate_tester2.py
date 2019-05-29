445c163c36ee26facf6dd6f762fdd54b6c346515
Andres Calderon <acald013@ucr.edu>
2019-05-29 09:42:14 -0700

    Tracking bug in area_interpolate when dataset has null intensive values...

--- Code/Validation/GeoPandas_area_interpolate_tester2.py
+++ Code/Validation/GeoPandas_area_interpolate_tester2.py
@@ -6,9 +6,87 @@ from shapely import wkt
 import numpy as np
 import pandas as pd
 import geopandas as gpd
-from tobler import area_tables, area_interpolate
 import sys
 import argparse
+
+def area_tables(source_df, target_df):
+    if _check_crs(source_df, target_df):
+        pass
+    else:
+        return None
+
+    n_s = source_df.shape[0]
+    n_t = target_df.shape[0]
+    _left = np.arange(n_s)
+    _right = np.arange(n_t)
+    source_df.loc[:, '_left'] = _left  # create temporary index for union
+    target_df.loc[:, '_right'] = _right # create temporary index for union
+    res_union = gpd.overlay(source_df, target_df, how='union')
+    n_u, _ = res_union.shape
+    SU = np.zeros((n_s, n_u)) # holds area of intersection of source geom with union geom
+    UT = np.zeros((n_u, n_t)) # binary table mapping union geom to target geom
+    for index, row in res_union.iterrows():
+        # only union polygons that intersect both a source and a target geometry matter 
+        if not np.isnan(row['_left']) and not np.isnan(row['_right']):
+            s_id = int(row['_left'])
+            t_id = int(row['_right'])
+            SU[s_id, index] = row['geometry'].area
+            UT[index, t_id] = 1
+    source_df.drop(['_left'], axis=1, inplace=True)
+    target_df.drop(['_right'], axis=1, inplace=True)
+    return SU, UT
+
+def _check_crs(source_df, target_df):
+    """check if crs is identical"""
+    if not (source_df.crs == target_df.crs):
+        print("Source and target dataframes have different crs. Please correct.")
+        return False
+    return True
+
+def area_interpolate(source_df, target_df, extensive_variables=[], intensive_variables=[], tables=None, allocate_total=True):
+    if tables is None:
+        SU, UT  = area_tables(source_df, target_df)
+    else:
+        SU, UT = tables
+    den = source_df['geometry'].area.values
+    if allocate_total:
+        den = SU.sum(axis=1)
+    den = den + (den==0)
+    weights = np.dot(np.diag(1/den), SU)
+
+    extensive = [] 
+    for variable in extensive_variables:
+        vals = _nan_check(source_df, variable)
+        estimates = np.dot(np.diag(vals), weights)
+        estimates = np.dot(estimates, UT)
+        estimates = estimates.sum(axis=0)
+        extensive.append(estimates)
+    extensive = np.array(extensive)
+
+    ST = np.dot(SU, UT)
+    area = ST.sum(axis=0)
+    den = np.diag(1./ (area + (area == 0)))
+    weights = np.dot(ST, den)
+    intensive = []
+    for variable in intensive_variables:
+        vals = _nan_check(source_df, variable)
+        vals.shape = (len(vals), 1)
+        est = (vals * weights).sum(axis=0)
+        intensive.append(est)
+    intensive = np.array(intensive)
+
+    return (extensive, intensive)
+
+def _nan_check(df, column):
+    """Check if variable has nan values.
+    Warn and replace nan with 0.0.
+    """
+    values = df[column].values
+    if np.any(np.isnan(values)):
+        wherenan = np.isnan(values)
+        values[wherenan] = 0.0
+        print('nan values in variable: {var}, replacing with 0.0'.format(var=column))
+    return values
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--source", "-s", help="Source polygon file...")
