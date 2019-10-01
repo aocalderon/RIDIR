419f5b6fc8e2bc8b39adff97f3e008a0e9c07ed0
Andres Calderon <acald013@ucr.edu>
2019-10-01 14:31:52 -0700

    Working on merged DCEL...

--- Code/DCEL/src/main/scala/DCELMerger.scala
+++ Code/DCEL/src/main/scala/DCELMerger.scala
@@ -1,8 +1,3 @@
-/***
- * Implementation based on https://github.com/anglyan/dcel/blob/master/dcel/dcel.py 
- * with some updates and additions...
- ***/
-
 import org.slf4j.{LoggerFactory, Logger}
 import org.rogach.scallop._
 import org.apache.spark.sql.{SparkSession, Dataset}
@@ -17,7 +12,7 @@ import org.datasyslab.geospark.serde.GeoSparkKryoRegistrator
 import org.datasyslab.geospark.spatialPartitioning.quadtree.{QuadTreePartitioner, StandardQuadTree, QuadRectangle}
 import com.vividsolutions.jts.operation.buffer.BufferParameters
 import com.vividsolutions.jts.geom.GeometryFactory
-import com.vividsolutions.jts.geom.{Geometry, Envelope, Coordinate,  Polygon, LinearRing, LineString}
+import com.vividsolutions.jts.geom.{Geometry, Envelope, Coordinate,  Polygon, LinearRing, LineString, Point}
 import com.vividsolutions.jts.geom.impl.CoordinateArraySequence
 import com.vividsolutions.jts.algorithm.{RobustCGAlgorithms, CGAlgorithms}
 import com.vividsolutions.jts.io.WKTReader
@@ -32,6 +27,10 @@ object DCELMerger{
   private val precision: Double = 0.001
   private val startTime: Long = 0L
 
+  implicit class Crossable[X](xs: Traversable[X]) {
+    def cross[Y](ys: Traversable[Y]) = for { x <- xs; y <- ys } yield (x, y)
+  }
+  
   def clocktime = System.currentTimeMillis()
 
   def log(msg: String, timer: Long, n: Long, status: String): Unit ={
@@ -66,7 +65,7 @@ object DCELMerger{
     new Polygon(ring, null, geofactory)
   }
 
-  def buildLocalDCEL(edges: List[Edge], tag: String = ""): LocalDCEL = {
+  def buildLocalDCEL2(edges: List[Edge], tag: String = ""): LocalDCEL = {
     var half_edgeList = new ArrayBuffer[Half_edge]()
     var faceList = new ArrayBuffer[Face]()
 
@@ -150,11 +149,116 @@ object DCELMerger{
     LocalDCEL(half_edgeList.toList, faceList.toList, vertexList.toList)
   }
 
+  def buildLocalDCEL(edges: List[Edge], tag: String = ""): LocalDCEL = {
+    var half_edgeList = new ArrayBuffer[Half_edge]()
+    var faceList = new ArrayBuffer[Face]()
+
+    // Step 1. Vertex set creation...
+    var timer = clocktime
+    var vertexList = edges.flatMap(e => List(e.v1, e.v2)).toSet
+    logger.info(s"Vertex set... ${(clocktime - timer) / 1000.0}")
+
+    // Step 2.  Edge set creation with left and right labels...
+    timer = clocktime
+    val r = edges.cross(edges.map(e => Edge(e.v2, e.v1, "", e.id)))
+      .filter(e => e._1.id < e._2.id)
+      .filter(e => e._1.v1 == e._2.v1 && e._1.v2 == e._2.v2)
+    val toUpdate = r.map{ p =>
+      p._1.r = p._2.id
+      p._1
+    }
+    val toDelete = r.map(_._1) ++ r.map(e => Edge(e._2.v2, e._2.v1, "", e._2.id))
+    val edgesSet = edges.filterNot(toDelete.toSet).union(toUpdate.toList)
+    logger.info(s"Edge set... ${(clocktime - timer) / 1000.0}")
+
+    // Step 3. Half-edge list creation with twins and vertices assignments...
+    timer = clocktime
+    edgesSet.foreach{ edge =>
+      val h1 = Half_edge(edge.v1, edge.v2)
+      h1.label = edge.l
+      h1.id = edge.l
+      h1.tag = tag
+      val h2 = Half_edge(edge.v2, edge.v1)
+      h2.label = edge.r
+      h2.id = edge.r
+      h2.tag = tag
+
+      h1.twin = h2
+      h2.twin = h1
+
+      vertexList.find(_.equals(edge.v2)).get.half_edges += h1
+      vertexList.find(_.equals(edge.v1)).get.half_edges += h2
+
+      half_edgeList += h2
+      half_edgeList += h1
+    }
+    logger.info(s"Half-edge set... ${(clocktime - timer) / 1000.0}")
+
+    // Step 4. Identification of next and prev half-edges...
+    timer = clocktime
+    vertexList.map{ vertex =>
+      val sortedIncidents = vertex.half_edges.toList.sortBy(_.angle)(Ordering[Double].reverse)
+      val size = sortedIncidents.size
+
+      if(size < 2){
+        logger.error("Badly formed dcel: less than two hedges in vertex")
+      }
+      
+      for(i <- 0 until (size - 1)){
+        var current = sortedIncidents(i)
+        var next = sortedIncidents(i + 1)
+        current = half_edgeList.find(_.equals(current)).get
+        next = half_edgeList.find(_.equals(next)).get
+        current.next   = next.twin
+        next.twin.prev = current
+      }
+      var current = sortedIncidents(size - 1)
+      var next = sortedIncidents(0)
+      current = half_edgeList.find(_.equals(current)).get
+      next = half_edgeList.find(_.equals(next)).get
+      current.next   = next.twin
+      next.twin.prev = current
+    }
+    logger.info(s"Next and prev set... ${(clocktime - timer) / 1000.0}")
+
+    // Step 5. Face assignment...
+    timer = clocktime
+    var temp_half_edgeList = new ArrayBuffer[Half_edge]()
+    temp_half_edgeList ++= half_edgeList
+    for(temp_hedge <- temp_half_edgeList){
+      val hedge = half_edgeList.find(_.equals(temp_hedge)).get
+      if(hedge.face == null){
+        val f = Face(hedge.label)
+        f.outerComponent = hedge
+        f.outerComponent.face = f
+        f.id = hedge.id
+        var h = hedge
+        do{
+          half_edgeList.find(_.equals(h)).get.face = f
+          h = h.next
+        }while(h != f.outerComponent)
+        faceList += f
+      }
+    }
+
+    val faces = faceList.groupBy(_.id).map{ case (id, faces) =>
+      val f = faces.toList.sortBy(_.faceArea())(Ordering[Double].reverse)
+      val head = f.head
+      val tail = f.tail
+      head.innerComponent = tail
+
+      head
+    }
+    logger.info(s"Face set... ${(clocktime - timer) / 1000.0}")
+
+    LocalDCEL(half_edgeList.toList, faces.toList, vertexList.toList, edgesSet)
+  }
+
   def readPolygons(spark: SparkSession, input: String, offset: Int): SpatialRDD[Geometry] = {
     val polygonRDD = new SpatialRDD[Geometry]()
     val polygons = spark.read.option("header", "false").option("delimiter", "\t")
       .csv(input).rdd.zipWithUniqueId().map{ row =>
-        val polygon = reader.read(row._1.getString(offset))//.asInstanceOf[Polygon]
+        val polygon = new WKTReader(geofactory).read(row._1.getString(offset))
         val userData = (0 until row._1.size).filter(_ != offset).map(i => row._1.getString(i)).mkString("\t")
         polygon.setUserData(userData)
         polygon
@@ -162,6 +266,21 @@ object DCELMerger{
     polygonRDD.setRawSpatialRDD(polygons)
     polygonRDD.rawSpatialRDD.cache()
     polygonRDD
+  }
+
+  def getRings(polygon: Polygon): List[Array[Coordinate]] = {
+    var exteriorCoords = polygon.getExteriorRing.getCoordinateSequence.toCoordinateArray()
+    if(!CGAlgorithms.isCCW(exteriorCoords)) { exteriorCoords = exteriorCoords.reverse }
+    val outerRing = List(exteriorCoords)
+
+    val nInteriorRings = polygon.getNumInteriorRing
+    val innerRings = (0 until nInteriorRings).map{ i => 
+      var interiorCoords = polygon.getInteriorRingN(i).getCoordinateSequence.toCoordinateArray()
+      if(CGAlgorithms.isCCW(interiorCoords)) { interiorCoords = interiorCoords.reverse }
+      interiorCoords
+    }.toList
+
+    outerRing ++ innerRings
   }
 
   def clipPolygons(polygons: SpatialRDD[Geometry], grids: Map[Int, Envelope]): RDD[Polygon] = {
@@ -174,7 +293,7 @@ object DCELMerger{
         var ps = new ListBuffer[Polygon]()
         polygons.foreach { to_clip =>
           val label = to_clip.getUserData.toString
-          val geoms = clipper.clip(to_clip, true)
+          val geoms = clipper.clipSafe(to_clip, true, 0.001)
           for(i <- 0 until geoms.getNumGeometries){
             val geom = geoms.getGeometryN(i)
             if(geom.getGeometryType == "Polygon" && !geom.isEmpty()){
@@ -194,24 +313,27 @@ object DCELMerger{
   def buildDCEL(clippedPolygons: RDD[Polygon], tag: String = ""): RDD[LocalDCEL]= {
     val dcel = clippedPolygons.mapPartitionsWithIndex{ (i, polygons) =>
       val edges = polygons.flatMap{ p =>
-        val ring = p.getExteriorRing.getCoordinateSequence.toCoordinateArray()
-        val orientation = CGAlgorithms.isCCW(ring)
-        var coords = List.empty[Coordinate]
-        if(orientation){
-          coords = ring.toList
-        } else {
-          coords = ring.reverse.toList
+        var edges = ListBuffer[Edge]()
+        for(ring <- getRings(p)){
+          val coords = ring.toList
+          val segments = coords.zip(coords.tail)
+          segments.foreach{ segment =>
+            // Assuming id value is in first position...
+            val id = p.getUserData.toString().split("\t")(0)
+            val v1 = Vertex(segment._1.x, segment._1.y)
+            val v2 = Vertex(segment._2.x, segment._2.y)
+            edges += Edge(v1, v2, p.getUserData.toString(), id)
+          }
         }
-        val segments = coords.zip(coords.tail)
-        segments.map{ segment =>
-          val v1 = Vertex(segment._1.x, segment._1.y)
-          val v2 = Vertex(segment._2.x, segment._2.y)
-          Edge(v1, v2, p.getUserData.toString())
-        }
+        edges.toList
       }
+      val start = clocktime
       val dcel = buildLocalDCEL(edges.toList, tag)
-      dcel.id = i
+      val end = clocktime
       dcel.tag = tag
+      dcel.id = i
+      dcel.nEdges = dcel.edges.size
+      dcel.executionTime = end - start
       List(dcel).toIterator
     }
     dcel
@@ -249,9 +371,9 @@ object DCELMerger{
       .config("spark.serializer",classOf[KryoSerializer].getName)
       .config("spark.kryo.registrator", classOf[GeoSparkKryoRegistrator].getName)
       .config("spark.scheduler.mode", "FAIR")
-      .config("spark.cores.max", cores * executors)
-      .config("spark.executor.cores", cores)
-      .master(master)
+      //.config("spark.cores.max", cores * executors)
+      //.config("spark.executor.cores", cores)
+      //.master(master)
       .appName("DCEL")
       .getOrCreate()
     import spark.implicits._
@@ -283,33 +405,21 @@ object DCELMerger{
     log(stage, timer, 0, "START")
     val boundary1 = polygonsA.boundaryEnvelope
     val boundary2 = polygonsB.boundaryEnvelope
-    if(debug){ logger.info(s"Testing...") }
     val fullBoundary = envelope2Polygon(boundary1).union(envelope2Polygon(boundary2)).getEnvelopeInternal
-    val samplesA = polygonsA.rawSpatialRDD.rdd.sample(false, params.fraction(), 42).map(_.getEnvelopeInternal)
-    val samplesB = polygonsB.rawSpatialRDD.rdd.sample(false, params.fraction(), 42).map(_.getEnvelopeInternal)
-    val samples = samplesA.union(samplesB)
-
-    //samples.collect().map(p => envelope2Polygon(p).toText()).foreach(println)
-    if(debug){ logger.info(s"Sample' size: ${samples.count()}") }
-
-    val boundary = new QuadRectangle(fullBoundary)
-    val maxLevels = params.levels()
-    val maxEntriesPerNode = params.entries()
-    val quadtree = new StandardQuadTree[Geometry](boundary, 0, maxEntriesPerNode, maxLevels)
-    if(debug){ logger.info(s"Sample' size: ${samples.count()}") }
-    for(sample <- samples.collect()){
-      quadtree.insert(new QuadRectangle(sample), null)
-    }
-    quadtree.assignPartitionIds()
-    val QTPartitioner = new QuadTreePartitioner(quadtree)
-    polygonsA.spatialPartitioning(QTPartitioner)
-    polygonsB.spatialPartitioning(polygonsA.getPartitioner)
-    val grids = quadtree.getAllZones.asScala.filter(_.partitionId != null)
-      .map(r => r.partitionId.toInt -> r.getEnvelope).toMap
+    val points = polygonsB.rawSpatialRDD.rdd.flatMap(p => p.getCoordinates.map(geofactory.createPoint))
+    val pointsRDD = new SpatialRDD[Point]()
+    pointsRDD.setRawSpatialRDD(points)
+    pointsRDD.analyze()
+    pointsRDD.boundaryEnvelope = fullBoundary
+    pointsRDD.spatialPartitioning(gridType, partitions)
+    polygonsA.spatialPartitioning(pointsRDD.getPartitioner)
+    polygonsB.spatialPartitioning(pointsRDD.getPartitioner)
+    val grids = pointsRDD.getPartitioner.getGrids.asScala.zipWithIndex.map(g => g._2 -> g._1).toMap
     log(stage, timer, grids.size, "END")
 
     if(debug) {
-      val gridsWKT = grids.values.map(g => s"${envelope2Polygon(g).toText()}\n")
+      val gridsWKT =  pointsRDD.partitionTree.getAllZones().asScala.filter(_.partitionId != null)
+        .map(z => s"${z.partitionId}\t${envelope2Polygon(z.getEnvelope).toText()}\n")
       val f = new java.io.PrintWriter("/tmp/dcelGrid.wkt")
       f.write(gridsWKT.mkString(""))
       f.close()
@@ -403,7 +513,7 @@ object DCELMerger{
       val anRDD = mergedDCEL.mapPartitionsWithIndex{ (i, dcels) =>
         val dcel = dcels.toList.head
         dcel.half_edges.map{ h =>
-          s"${h.v2.toWKT}\t${h.toWKT2}\t${h.angle}\t${dcel.partition}\t${i}\n"
+          s"${h.v2.toWKT}\t${h.toWKT}\t${h.angle}\t${dcel.partition}\t${i}\n"
         }.toIterator
       }.collect()
       val f = new java.io.PrintWriter("/tmp/hedges.wkt")
@@ -438,7 +548,7 @@ object DCELMerger{
       dcels.next().union()
         .map(f => (f.tag, f.toWKT))
         .toIterator
-    }.reduceByKey{ mergePolygons }.map(f => s"${f._1}\t${f._2}\n")
+    }.reduceByKey{ mergePolygons }.map(f => s"${f._1}\t${f._2}\n").cache()
     log(stage, timer, union.count(), "END")
     saveWKT(union, "/tmp/union.wkt")
 
@@ -449,7 +559,7 @@ object DCELMerger{
       dcels.next().intersection()
         .map(f => (f.tag, f.toWKT()))
         .toIterator
-    }.reduceByKey{ mergePolygons }.map(f => s"${f._1}\t${f._2}\n")
+    }.reduceByKey{ mergePolygons }.map(f => s"${f._1}\t${f._2}\n").cache()
     log(stage, timer, intersection.count(), "END")
     saveWKT(intersection, "/tmp/intersection.wkt")
 
@@ -460,7 +570,7 @@ object DCELMerger{
       dcels.next().symmetricDifference()
         .map(f => (f.tag, f.toWKT()))
         .toIterator
-    }.reduceByKey{ mergePolygons }.map(f => s"${f._1}\t${f._2}\n")
+    }.reduceByKey{ mergePolygons }.map(f => s"${f._1}\t${f._2}\n").cache()
     log(stage, timer, symmetric.count(), "END")
     saveWKT(symmetric, "/tmp/symmetric.wkt")
 
@@ -471,7 +581,7 @@ object DCELMerger{
       dcels.next().differenceA()
         .map(f => (f.tag, f.toWKT()))
         .toIterator
-    }.reduceByKey{ mergePolygons }.map(f => s"${f._1}\t${f._2}\n")
+    }.reduceByKey{ mergePolygons }.map(f => s"${f._1}\t${f._2}\n").cache()
     log(stage, timer, differenceA.count(), "END")
     saveWKT(differenceA, "/tmp/differenceA.wkt")
 
@@ -482,7 +592,7 @@ object DCELMerger{
       dcels.next().differenceB()
         .map(f => (f.tag, f.toWKT()))
         .toIterator
-    }.reduceByKey{ mergePolygons }.map(f => s"${f._1}\t${f._2}\n")
+    }.reduceByKey{ mergePolygons }.map(f => s"${f._1}\t${f._2}\n").cache()
     log(stage, timer, differenceB.count(), "END")
     saveWKT(differenceB, "/tmp/differenceB.wkt")
 
@@ -507,9 +617,9 @@ class DCELMergerConf(args: Seq[String]) extends ScallopConf(args) {
   val grid:       ScallopOption[String]  = opt[String]  (default = Some("QUADTREE"))
   val index:      ScallopOption[String]  = opt[String]  (default = Some("QUADTREE"))
   val partitions: ScallopOption[Int]     = opt[Int]     (default = Some(512))
-  val levels:     ScallopOption[Int]     = opt[Int]     (default = Some(16))
-  val entries:    ScallopOption[Int]     = opt[Int]     (default = Some(64))
-  val fraction:   ScallopOption[Double]  = opt[Double]  (default = Some(0.1))
+  val levels:     ScallopOption[Int]     = opt[Int]     (default = Some(128))
+  val entries:    ScallopOption[Int]     = opt[Int]     (default = Some(50))
+  val fraction:   ScallopOption[Double]  = opt[Double]  (default = Some(0.25))
   val local:      ScallopOption[Boolean] = opt[Boolean] (default = Some(false))
   val debug:      ScallopOption[Boolean] = opt[Boolean] (default = Some(false))
 
