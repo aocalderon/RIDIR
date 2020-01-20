d2575aea763403b302ff4f1ce4a759871b82921d
Andres Calderon <acald013@ucr.edu>
2020-01-20 15:28:22 -0800

    Setting Spark on yarn mode...

--- Code/DCEL/src/main/scala/DCELTester.scala
+++ Code/DCEL/src/main/scala/DCELTester.scala
@@ -174,7 +174,7 @@ object DCELTester{
       val f = faces.toArray.sortBy(_.faceArea())(Ordering[Double].reverse)
       val head = f.head
       val tail = f.tail
-      head.innerComponents = tail
+      head.innerComponents = tail.toVector
 
       head
     }
