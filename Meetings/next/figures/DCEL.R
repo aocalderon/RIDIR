757d95d0b0aa1b056613090d81e72081391818a5
Andres Calderon <andress.calderon@gmail.com>
2020-07-04 09:41:15 -0700

    Adding last meeting...

--- Meetings/next/figures/DCEL.R
+++ Meetings/next/figures/DCEL.R
@@ -1,57 +1,42 @@
 require(tidyverse)
 
-log = enframe(readLines("DCELMerger_CA_04.txt"))
-paramsPattern = "partitions|num-executors|executor-cores"
-getParams <- function(command){
-  params = str_trim(str_split(command, "--")[[1]])
-  params = params[grepl(paramsPattern, params)]
-  return(paste(params, collapse = " "))
+log = read_tsv("log.tsv")
+extract = log %>% filter(phaseName == "Extract DCELs") %>% arrange(duration)
+merge   = log %>% filter(phaseName == "Merge DCELs") %>% arrange(duration)
+
+getTimeArray <- function(log){
+  d1 = log$schedulerDelay 
+  d2 = log$gettingResultTime 
+  d3 = log$executorRunTime  
+  d4 = log$executorDeserializeTime 
+  d5 = log$resultSerializationTime 
+  d6 = log$shuffleWriteTime 
+  d7 = log$shuffleFetchWaitTime
+  log$Time = paste(d1,d2,d3,d4,d5,d6,d7)
+  log$Task = paste("Scheduler","Result","Compute","Deserialize","Serialize","ShuffleWrite","ShuffleRead")
+  return(log)
 }
 
-spark = log %>% filter(grepl(value, pattern = "SparkSubmit ")) %>% 
-  separate(value, into = c(NA, "appId", "command"), sep = "\\|")
-spark$params = spark$command %>% map(getParams)
-spark = spark %>% separate(params, into = c(NA, "Nodes", NA, "Cores", NA, "Partitions"), sep = " ") %>%
-  #mutate(appId = as.numeric(appId)) %>%
-  select(appId, Nodes, Cores, Partitions) 
+top = log %>%
+  mutate(duration = as.numeric(duration), index = as.factor(index)) %>% 
+  slice_max(duration, n = 100)
 
-fields = c("Timestamp","Tag1","appId","Nodes","Cores","Partitions","Stage","Time")
-mf = log %>% filter(grepl(value, pattern = "\\|DCELMerger\\|")) %>% 
-  separate(value, fields, sep = "\\|") %>%
-  mutate(Time = as.numeric(Time)) %>%
-  #mutate(appId = as.numeric(appId)) %>%
-  select(appId, Stage, Time)
+log = getTimeArray(top)
+data = log %>% separate_rows(Time, Task, sep = " ", convert = TRUE) %>%
+  select(appId, jobId, stageId, index, host, duration, phaseName, Task, Time) 
 
-data = mf %>% inner_join(spark, by = c("appId")) 
+data0 = data #%>% filter(phaseName == "Merge DCELs" & duration > 500)
+indexOrder = data0 %>% select(index) %>% distinct() %>% mutate(nIndex = as.numeric(index)) %>% 
+  arrange(nIndex) %>% select(index) %>% as.list()
+data0$index = factor(data0$index, levels = indexOrder$index)
 
-nStages = length(unique(data$Stage))
-data$stageId = rep(1:nStages, dim(data)[1] / nStages)
-stageOrder = data %>% select(stageId, Stage) %>% distinct() %>% arrange(stageId) %>% select(Stage) %>% as.list()
-data$Stage = factor(data$Stage, levels = stageOrder$Stage)
+p = ggplot(data=data0, aes(x = index, y = Time, fill = Task)) +
+  geom_bar(stat="identity",colour="black", size=0.1) +
+  coord_flip() +
+  facet_grid(host ~ ., scales = "free", space = "free") + 
+  scale_fill_manual(values = c("blue", "green", "cyan", "purple", "orange", "red", "yellow")) +
+  theme(strip.text.y = element_text(angle = 0)) +
+  ylab("Time [ms]") + xlab("Task Id") + labs(fill = "Time to")
 
-partitionsOrder = data %>% select(Partitions) %>% distinct() %>% mutate(nPartitions = as.numeric(Partitions)) %>% 
-  arrange(nPartitions) %>% select(Partitions) %>% as.list()
-data$Partitions = factor(data$Partitions, levels = partitionsOrder$Partitions)
-
-data = data %>% select(Stage, Partitions, Time) %>%
-  group_by(Stage, Partitions) %>% summarise(Time = mean(Time))
-
-data1 = data %>% group_by(Partitions, Stage) %>% summarise(Time = mean(Time)) %>%
-  filter(Stage != "Reading polygons A            ") %>%
-  filter(Stage != "Reading polygons B            ")
-
-
-p = ggplot(data = data1, aes(x = Stage, y = Time, fill = Partitions)) +
-  geom_bar(stat="identity", position=position_dodge(width = 0.75), width = 0.7) + 
-  theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
-  labs(x="Stages", y="Time [s]", title="Execution time by stages") 
 plot(p)
-ggsave("ByStages.pdf", width = 10, height = 7, device = "pdf")
-
-data2 = data1 %>% group_by(Partitions) %>% summarise(Time = sum(Time))
-p = ggplot(data = data2, aes(x = Partitions, y = Time)) +
-  geom_bar(stat="identity", position=position_dodge(width = 0.75), width = 0.7) + 
-  theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
-  labs(x="Partitions", y="Time [s]", title="Execution time") 
-plot(p)
-ggsave("ByPartitions.pdf", width = 10, height = 7, device = "pdf")
+ggsave("TopTasksHist.pdf", width = 10, height = 12, device = "pdf")
