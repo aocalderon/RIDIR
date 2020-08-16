d631cbd98276b9a72a5e026b9e64fc3b9f085bd4
Andres Calderon <andress.calderon@gmail.com>
2020-08-15 18:56:24 -0700

    Cleaning quadtree...

--- Code/DCEL/src/main/scala/quadtreeTest/CellManagerTest.scala
+++ Code/DCEL/src/main/scala/quadtreeTest/CellManagerTest.scala
@@ -2,47 +2,72 @@ import scala.collection.JavaConverters._
 import com.vividsolutions.jts.geom.{Envelope, Coordinate, LineString}
 import com.vividsolutions.jts.geom.GeometryFactory
 import com.vividsolutions.jts.io.WKTReader
+import org.rogach.scallop._
 import scala.io.Source
+import java.io.PrintWriter
 import edu.ucr.dblab.{StandardQuadTree, QuadRectangle}
 import CellManager._
 
 object CellManagerTest {
-  case class Cell(lineage: String, count: Int)
+  def save2(filename: String, content: Seq[String]): Unit = {
+    val f = new PrintWriter(filename)
+    f.write(content.mkString(""))
+    f.close
+  }
 
   def main(args: Array[String]): Unit = {
-    val path = "/home/acald013/RIDIR/Code/DCEL/src/test/resources"
+    val params = new CMTConf(args)
     val geofactory = new GeometryFactory()
     val reader = new WKTReader(geofactory)
-    val boundaryBuffer = Source.fromFile(s"${path}/envelope.tsv")
+
+    // Reading boundary file...
+    val boundaryBuffer = Source.fromFile(params.boundary())
     val boundaryWKT = boundaryBuffer.getLines.next
     boundaryBuffer.close
+    val boundary = reader.read(boundaryWKT).getEnvelopeInternal
 
-    val boundary = reader.read(boundaryWKT)
-
-    val cellsBuffer = Source.fromFile(s"${path}/quadtree.tsv")
-    val cells = cellsBuffer.getLines.map{ line =>
+    // Reading lineages file...
+    val lineagesBuffer = Source.fromFile(params.input())
+    val lineages = lineagesBuffer.getLines.map{ line =>
         val arr = line.split("\t")
-        Cell(arr(0), arr(1).toInt)
+        arr(0)
       }.toList
-    cellsBuffer.close
+    lineagesBuffer.close
 
-    val lineages = cells.map(_.lineage)
-    lineages.take(5).foreach{println}
-    val quadtree = Quadtree.create[LineString](boundary.getEnvelopeInternal, lineages)
-
-    val MBuffer = Source.fromFile(s"${path}/MB.tsv")
+    // Reading map file...
+    val MBuffer = Source.fromFile(params.map())
     val M = MBuffer.getLines.map{ line =>
       val arr = line.split("\t")
       (arr(0).toInt -> arr(1).toInt)
     }.toMap
 
-    val grids = quadtree.getLeafZones.asScala.map{ leaf =>
-      leaf.partitionId.toInt -> leaf
-    }.toMap
+    val quadtree = Quadtree.create[LineString](boundary, lineages)
 
-    cleanQuadtree(quadtree, M)
+    val clean = cleanQuadtree(quadtree, M)
 
-    //getNextCellWithEdges2(M: Map[Int, Int], quadtree: StandardQuadTree[LineString], grids: Map[Int, QuadRectangle])
+    val f = new PrintWriter(params.output())
+    val cleanWKT = clean.getLeafZones.asScala.map{ leaf =>
+      s"${envelope2Polygon(leaf.getEnvelope())}\t${leaf.partitionId}\n"
+    }
+    f.write(cleanWKT.mkString(""))
+    f.close
+    println(s"${params.output()} saved [${cleanWKT.length} records].")
 
+    val i = new PrintWriter("/tmp/edgesInput.wkt")
+    val quadtreeWKT = quadtree.getLeafZones.asScala.map{ leaf =>
+      s"${envelope2Polygon(leaf.getEnvelope())}\t${leaf.partitionId}\n"
+    }
+    i.write(quadtreeWKT.mkString(""))
+    i.close
+    println(s"/tmp/edgesInput.wkt saved [${quadtreeWKT.length} records].")
   }
+}
+
+class CMTConf(args: Seq[String]) extends ScallopConf(args) {
+  val input: ScallopOption[String] = opt[String] (required = true)
+  val boundary: ScallopOption[String] = opt[String] (required = true)
+  val map: ScallopOption[String] = opt[String] (required = true)
+  val output: ScallopOption[String] = opt[String](default = Some("/tmp/edgesOutput.wkt"))
+
+  verify()
 }
