d4fc19d7a4c0c2bd71f6cac1610a10dd63d4ce09
Andres Calderon <andress.calderon@gmail.com>
2020-09-13 17:13:36 -0700

    Fixing bug on cell intersections...

--- Code/DCEL/src/main/scala/DCELTester2.scala
+++ Code/DCEL/src/main/scala/DCELTester2.scala
@@ -28,9 +28,16 @@ import DCELMerger._
 
 object DCELTester2 {
   implicit val logger: Logger = LoggerFactory.getLogger("myLogger")
-  private val model: PrecisionModel = new PrecisionModel(1000)
-  val geofactory: GeometryFactory = new GeometryFactory(model);
-  val precision: Double = 1 / model.getScale
+
+  def round(number: Double, scale: Double = 1000.0): Double =
+    Math.round(number * scale) / scale;
+  def roundEnvelope(envelope: Envelope, scale: Double = 1000.0): Envelope = {
+    val e = round(envelope.getMinX, scale)
+    val w = round(envelope.getMaxX, scale)
+    val s = round(envelope.getMinY, scale)
+    val n = round(envelope.getMaxY, scale)
+    new Envelope(e, w, s, n)
+  }
 
   def main(args: Array[String]) = {
     // Starting session...
@@ -49,17 +56,22 @@ object DCELTester2 {
     logger.info("Starting session... Done!")
 
     import scala.io.Source
+    val model = new PrecisionModel(1000)
+    val geofactory = new GeometryFactory(model);
     val reader = new WKTReader(geofactory)
-    val path = "/home/acald013/RIDIR/Code/Scripts/gadm"
+    val home = System.getProperty("user.home")
+    val path = s"${home}/RIDIR/Code/Scripts/gadm"
     val boundaryBuff = Source.fromFile(s"${path}/boundary.wkt")
     val envelopeWKT = boundaryBuff.getLines.next
     val boundary = reader.read(envelopeWKT).getEnvelopeInternal
     boundaryBuff.close
-    val quadtreeBuff = Source.fromFile(s"${path}/quadtree.wkt")
 
+    val quadtreeBuff = Source.fromFile(s"${path}/quadtree.wkt")
     val quads = quadtreeBuff.getLines.map{ line =>
+      val model = new PrecisionModel(1000)
+      val geofactory = new GeometryFactory(model);
       val arr = line.split("\t")
-      val polygon = reader.read(arr(0)).asInstanceOf[Polygon]
+      val polygon = reader.read(arr(0))
       val lineage = arr(1)
       val id = arr(2).toInt
       
@@ -70,7 +82,7 @@ object DCELTester2 {
     val cells = quadtree.getLeafZones.asScala.map{ cell =>
       val id = cell.partitionId.toInt
       val lineage = cell.lineage
-      val envelope = cell.getEnvelope
+      val envelope = roundEnvelope(cell.getEnvelope)
       val r = new org.datasyslab.geospark.spatialPartitioning.quadtree.QuadRectangle(envelope)
       r.lineage = lineage
       r.partitionId = id
@@ -78,10 +90,12 @@ object DCELTester2 {
     }.toMap
     val grids = spark.sparkContext
       .broadcast{
-        cells.toSeq.sortBy(_._1).map{ case (i,q) => envelope2polygon(q.getEnvelope)}
+        cells.toSeq.sortBy(_._1).map{ case (i,q) =>
+          envelope2polygon(q.getEnvelope)
+        }
       }
     
-    val edgesRDD_prime = spark.read.textFile(s"file://${path}/sample.tsv").rdd
+    val edgesRDD_prime = spark.read.textFile(s"file://${path}/demo.tsv").rdd
       .mapPartitionsWithIndex{ case(index, lines) =>
         val reader = new WKTReader(geofactory)
         lines.map{ line =>
@@ -115,6 +129,10 @@ object DCELTester2 {
       val pid = index2pid(index)
       val cell = envelope2polygon(cells(pid).getEnvelope)
       val gCellA = cell2gedges(cell)
+      val cellEnvelope = cell.getEnvelopeInternal
+
+      val edgesA = SweepLine.getEdgesOnCell(edges, cellEnvelope)
+      edgesA.foreach{println}
 
       val Ah = SweepLine.getGraphEdgeIntersections(gA, gCellA).flatMap{_.getLineStrings}
       val At = transform(Ah, cell)
@@ -123,12 +141,19 @@ object DCELTester2 {
       val Af = filter(Ap)
       val dcelA = getLDCEL(Af, index, cells, grids, true)
 
-      val r = (index, dcelA)
+      val r = (index, dcelA, edgesA)
       Iterator(r)
     }.cache
     val n = dcels.count()
 
-    println(n)
+    save{"/tmp/edgesEdges.wkt"}{
+      edgesRDD.map{ edge =>
+        val wkt = edge.toText
+        val data = edge.getUserData
+
+        s"$wkt\t$data\n"
+      }.collect
+    }
     save{"/tmp/edgesFaces.wkt"}{
       dcels.mapPartitionsWithIndex{ (index, dcels) =>
         val dcel = dcels.next
@@ -141,7 +166,45 @@ object DCELTester2 {
         }.toIterator
       }.collect
     }
- 
+    save{"/tmp/edgesHedges.wkt"}{
+      dcels.mapPartitionsWithIndex{ (index, dcels) =>
+        val dcel = dcels.next
+
+        dcel._2.half_edges.map{ hedge =>
+          val id = hedge.id
+          val wkt = hedge.toLineString.toText
+
+          s"$wkt\t$id\t$index\n"
+        }.toIterator
+      }.collect
+    }
+    save{"/tmp/edgesVertices.wkt"}{
+      dcels.mapPartitionsWithIndex{ (index, dcels) =>
+        val dcel = dcels.next
+
+        dcel._2.vertices.map{ vertex =>
+          val wkt = vertex.toWKT
+
+          s"$wkt\t$index\n"
+        }.toIterator
+      }.collect
+    }
+    save{"/tmp/edgesAh.wkt"}{
+      dcels.mapPartitionsWithIndex{ (index, dcels) =>
+        val dcel = dcels.next
+
+        dcel._3.map{ wkt =>
+          //val coords = x.getCoordinates
+          //val n = coords.length
+          //val wkt = geofactory.createLineString(coords)
+
+          s"$wkt\n"          
+        }.toIterator
+      }.collect
+    }
+    
     spark.close
   }
+
+  
 }
