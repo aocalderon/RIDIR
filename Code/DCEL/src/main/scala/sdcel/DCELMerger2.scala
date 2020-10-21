343d8958e0fe683505544f35aeba0a5950f678dc
Andres Calderon <andress.calderon@gmail.com>
2020-10-21 08:53:15 -0700

    Extracting intersection points during DCELs merging...

--- Code/DCEL/src/main/scala/sdcel/DCELMerger2.scala
+++ Code/DCEL/src/main/scala/sdcel/DCELMerger2.scala
@@ -4,6 +4,7 @@ import com.vividsolutions.jts.geom.LineString
 import com.vividsolutions.jts.geom.{PrecisionModel, GeometryFactory}
 import org.apache.spark.serializer.KryoSerializer
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.TaskContext
 import org.datasyslab.geospark.serde.GeoSparkKryoRegistrator
 import org.slf4j.{Logger, LoggerFactory}
 import edu.ucr.dblab.sdcel.quadtree._
@@ -30,20 +31,37 @@ object DCELMerger2 {
     // Reading data...
     val (edgesRDDA, quadtree, cells) = readEdges(params.input1())
     val edgesRDDB = readEdges(params.input2(), quadtree)
+    edgesRDDA.persist()
+    val nEdgesRDDA = edgesRDDA.count()
+    edgesRDDB.persist()
+    val nEdgesRDDB = edgesRDDB.count()
     logger.info("Reading data... Done!")
 
     // Getting LDCELs...
     val dcelsA = getLDCELs(edgesRDDA, cells).persist()
     val nA = dcelsA.count()
     logger.info("Getting LDCELs for A done!")
-    val dcelsB = getLDCELs(edgesRDDA, cells).persist()
+    val dcelsB = getLDCELs(edgesRDDB, cells).persist()
     val nB = dcelsB.count()
     logger.info("Getting LDCELs for B done!")
 
     // Merging DCELs...
-    dcelsA.zipPartitions(dcelsB, preservesPartitioning=true){ (iterA, iterB) =>
+    val dcel = dcelsA.zipPartitions(dcelsB, preservesPartitioning=true){ (iterA, iterB) =>
+      val partitionId = TaskContext.getPartitionId
+      val t0 = clocktime
+
+      //val hedgesA = setTwins(iterA.next.flatMap(_.getNexts).toList)
+      //val hedgesB = setTwins(iterB.next.flatMap(_.getNexts).toList)
+      
+
+      val t1 = clocktime
+      val time = "%.2f".format((t1 - t0) / 1000.0)
+      logger.info(s"Partition $partitionId in $time s")
+
       iterA
-    }
+    }.persist()
+    val nDcel = dcel.count()
+    logger.info("Merging DCELs... done!")
 
     if(params.debug()){
       save{"/tmp/edgesCells.wkt"}{
@@ -84,6 +102,35 @@ object DCELMerger2 {
      
     spark.close
   }
+
+  def setTwins(hedges: List[Half_edge])
+    (implicit geofactory: GeometryFactory): List[Half_edge] = {
+
+    case class H(vertex: Vertex, hedge: Half_edge, angle: Double)
+    val Hs = hedges.flatMap{ h =>
+      List(
+        H(h.orig, h, h.angleAtOrig),
+        H(h.dest, h, h.angleAtDest)
+      )
+    }
+    val grouped = Hs.groupBy(h => (h.vertex, h.angle)).values.foreach{ hList =>
+      val (h0, h1) = if(hList.size == 1) {
+        val h0 = hList(0).hedge
+        val h1 = h0.reverse
+        (h0, h1)
+      } else {
+        val h0 = hList(0).hedge
+        val h1 = hList(1).hedge
+        (h0, h1)
+      }
+
+      h0.twin = h1
+      h1.twin = h0
+    }
+
+    hedges
+  }
+  
 
   def save(filename: String)(content: Seq[String]): Unit = {
     val start = clocktime
