9560b3d7595c5a167b551bd69c9b430f278f8a49
Andres Calderon <andress.calderon@gmail.com>
2020-11-11 11:43:39 -0800

    Testing performance in local mode...

--- Code/DCEL/tester.sh
+++ Code/DCEL/tester.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-JAR="$HOME/RIDIR/Code/DCEL/target/scala-2.11/dcel_2.11-0.1.jar"
+JAR="$HOME/RIDIR/Code/DCEL/target/scala-2.11/sdcel_2.11-0.1.jar"
 LIBSPATH="$HOME/Spark/2.4/jars"
 LIBS="${LIBSPATH}/geospark-1.2.0.jar:${LIBSPATH}/slf4j-api-1.7.16.jar"
 CLASS="edu.ucr.dblab.sdcel.$1"
 
-scala -cp $LIBS:$JAR $CLASS $2 $3
+scala -cp $LIBS:$JAR $CLASS $2 $3 
