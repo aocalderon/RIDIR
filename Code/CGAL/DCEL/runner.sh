306594f1c3f2b367a844b01eeb353a6a2d523893
Andres Calderon <acald013@ucr.edu>
2021-01-10 09:45:38 -0800

    Running sequential dcel experiments...

--- Code/CGAL/DCEL/runner.sh
+++ Code/CGAL/DCEL/runner.sh
@@ -3,5 +3,6 @@
 N=$1
 
 for i in $(seq 1 $N); do
-    ./dcel -a ~/Datasets/WKT/CaliA.wkt -b ~/Datasets/WKT/CaliB.wkt
+    #./dcel -a ~/Datasets/WKT/CaliA.wkt -b ~/Datasets/WKT/CaliB.wkt -d
+    ./dcel -a /home/acald013/Datasets/WKT/gadm/gadmA.wkt -b /home/acald013/Datasets/WKT/gadm/gadmB.wkt
 done    
