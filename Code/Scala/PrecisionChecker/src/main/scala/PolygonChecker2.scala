1ff30de23a4975143084e823b9900500eb132831
Andres Calderon <andress.calderon@gmail.com>
2021-08-16 10:47:38 -0700

    Working on polygon checker...

--- Code/Scala/PrecisionChecker/src/main/scala/PolygonChecker2.scala
+++ Code/Scala/PrecisionChecker/src/main/scala/PolygonChecker2.scala
@@ -1,5 +1,6 @@
 import com.vividsolutions.jts.geom.{PrecisionModel, GeometryFactory, Polygon}
 import com.vividsolutions.jts.io.WKTReader
+import com.vividsolutions.jts.index.strtree.STRtree
 
 import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.rdd.RDD
@@ -16,7 +17,7 @@ import collection.JavaConverters._
 import scala.io.Source
 import java.io.PrintWriter
 
-object PolygonChecker {
+object PolygonChecker2 {
   def main(args:Array[String]) = {
     val scale = 1000.0
     implicit val model = new PrecisionModel(scale)
@@ -36,7 +37,7 @@ object PolygonChecker {
     val polysARDD = new SpatialRDD[Polygon]()
     polysARDD.setRawSpatialRDD(polysARaw)
     polysARDD.analyze()
-    polysARDD.spatialPartitioning(GridType.QUADTREE, 2)
+    polysARDD.spatialPartitioning(GridType.QUADTREE, 40)
     val polysA = polysARDD.spatialPartitionedRDD.rdd.cache
     println("A read")
   
@@ -56,39 +57,18 @@ object PolygonChecker {
     polysARDD.buildIndex(IndexType.QUADTREE, buildOnSpatialPartitionedRDD)
     val result = JoinQuery.SpatialJoinQueryFlat(polysARDD, polysBRDD, usingIndex, considerBoundaryIntersection)
 
-    val resRDD = polysA.zipPartitions(polysB, preservesPartitioning=true){ (itA, itB) =>
-      val psA = itA.toList
-      val psB = itB.toList
+    val data = result.rdd.map{ case(a,b) =>
+      a.normalize()
+      b.normalize()
+      val ee1 = a.equalsExact(b, 0.1)
+      val labA = a.getUserData.toString
+      val labB = b.getUserData.toString
 
-      val res = for{
-        a <- psA
-        b <- psB if{
-          try{
-            a.intersects(b)
-          } catch {
-            case e: com.vividsolutions.jts.geom.TopologyException => false
-          }
-        }
-      } yield {
-        a.normalize()
-        b.normalize()
-        val ee1 = a.equalsExact(b, 0.1)
-        val labA = a.getUserData.toString
-        val labB = b.getUserData.toString
+      //if(ee1) print(".")
 
-        //if(ee1) print(".")
-
-        (labA, labB, ee1)
-      }
-
-      res.toIterator
-    }
+      (labA, labB, ee1)
+    }.filter{ case(a,b,r1) => r1 }
     println("Join done")
-
-    //resRDD.foreach(println)
-
-    val data = resRDD.filter{ case(a,b,r1) => r1 }
-    println("Filter done")
 
     write("/tmp/results.txt",
       data.mapPartitionsWithIndex{ (pid, it) =>
@@ -104,6 +84,23 @@ object PolygonChecker {
   }
 
   def read(filename: String)(implicit spark: SparkSession, geofactory: GeometryFactory)
+      : RDD[Polygon] = {
+    val polys = spark.read.option("delimiter","\t").option("header","false")
+      .csv(filename).rdd.mapPartitions{ it =>
+        val reader = new WKTReader(geofactory)
+        it.map{ row =>
+          val wkt = row.getString(0)
+          val lab = row.getString(1)
+          val poly = reader.read(wkt).asInstanceOf[Polygon]
+          poly.setUserData(lab)
+          poly
+        }
+    }
+    polys
+  }
+
+  def read(filename: String, tree: STRtree)
+    (implicit spark: SparkSession, geofactory: GeometryFactory)
       : RDD[Polygon] = {
     val polys = spark.read.option("delimiter","\t").option("header","false")
       .csv(filename).rdd.mapPartitions{ it =>
