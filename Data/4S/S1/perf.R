996027d5e7b4cc63e792c80b6ff6db5fe8370e07
Andres Calderon <andress.calderon@gmail.com>
2022-03-04 10:17:26 -0800

    Plotting S1 and S2...

---
+++ Data/4S/S1/perf.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+
+data0 = enframe(read_lines("perf.txt"), value="line")
+
+data1 = data0 %>%
+  filter(str_detect(line, 'TIME')) 
+
+fields = c("ts","start","appId","time","tag","stage","partitions")
+data2 = data1 %>% 
+  separate(sep = "\\|", col = "line", into = fields, extra = "drop") %>%
+  filter(stage == "layer1" | stage == "layer2" | stage == "overlay") %>%
+  select(time, stage, partitions, appId) %>%
+  mutate(time = as.numeric(time) / 1000.0) %>%
+  mutate(partitions = fct_relevel(partitions, "500", "750", "1000", "2000", "3000", "4000", "5000"))
+
+data3 = data2 %>%
+  group_by(partitions, stage) %>% summarise(time = mean(time)) 
+
+p = ggplot(data3, aes(x = partitions, y = time, fill = stage)) + 
+  geom_col(width = 0.7, position="dodge") + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  labs(x="Number of partitions", y="Time [s]", title=paste0("Performance 1/4 census dataset for SDCEL computation"))
+plot(p)
+
+ggsave(paste0("perf.pdf"), width = 8, height = 5)
