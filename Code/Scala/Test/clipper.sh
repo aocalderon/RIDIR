ec17ed39d40c4f3b9eac05b763d8145501bdc269
Andres Calderon <acald013@ucr.edu>
2022-03-17 17:17:15 -0700

    Working on script to clip polygons...

--- Code/Scala/Test/clipper.sh
+++ Code/Scala/Test/clipper.sh
@@ -6,13 +6,16 @@ CORES=8
 DMEMORY=12g
 EMEMORY=30g
 SPARK_JARS=$HOME/Spark/2.4/jars/
-CLASS_JAR=$HOME/RIDIR/Code/Scala/Test/target/scala-2.11/tester_2.11-0.1.jar 
+CLASS_JAR=$HOME/RIDIR/Code/Scala/Test/target/scala-2.11/tester_2.11-0.1.jar
+CLASS_NAME="edu.ucr.dblab.Clipper"
 LOG_FILE=$HOME/Spark/2.4/conf/log4j.properties
 
-THE_INPUT="hdfs:///user/acald013/CA/CGAL/B.wkt"
-#THE_CLIP="Polygon ((-150066.4127 -605327.0480, 310002.8938 -605327.0480, 310002.8938 -77440.1680, -150066.4127 -77440.1680, -150066.4127 -605327.0480))"
-THE_OUTPUT="/tmp/edgesBS.wkt"
-THE_PARTITIONS=1024
+THE_INPUT=$1
+THE_REGION=$2
+THE_OUTPUT=$3
+DEFAULTVALUE=""
+THE_HDFS="${4:-$DEFAULTVALUE}"
+THE_PARTITIONS=16
 
 spark-submit \
     --files $LOG_FILE \
@@ -20,5 +23,5 @@ spark-submit \
     --jars ${SPARK_JARS}geospark-1.2.0.jar,${SPARK_JARS}geospark-sql_2.3-1.2.0.jar,${SPARK_JARS}geospark-viz_2.3-1.2.0.jar,${SPARK_JARS}scallop_2.11-3.1.5.jar,${SPARK_JARS}proj4j-1.1.1.jar \
     --master $MASTER --deploy-mode client \
     --num-executors $EXECUTORS --executor-cores $CORES --executor-memory $EMEMORY --driver-memory $DMEMORY \
-    --class Clipper $CLASS_JAR --input $THE_INPUT --output $THE_OUTPUT --partitions $THE_PARTITIONS --precision 10000.0
+    --class $CLASS_NAME $CLASS_JAR --input $THE_INPUT --region $THE_REGION --output $THE_OUTPUT --partitions $THE_PARTITIONS $THE_HDFS
 
--- Code/Scala/Test/src/main/scala/Clipper.scala
+++ Code/Scala/Test/src/main/scala/Clipper.scala
@@ -1,3 +1,5 @@
+package edu.ucr.dblab
+
 import org.slf4j.{LoggerFactory, Logger}
 import org.apache.spark.serializer.KryoSerializer
 import org.apache.spark.sql.{SparkSession, SaveMode}
@@ -18,14 +20,15 @@ object Clipper{
     implicit val logger: Logger = LoggerFactory.getLogger("myLogger")
     logger.info("Starting session...")
     val params = new ClipperConf(args)
+    val delimiter = params.delimiter()
     implicit val spark = SparkSession.builder()
       .config("spark.serializer",classOf[KryoSerializer].getName)
       .config("spark.kryo.registrator", classOf[GeoSparkVizKryoRegistrator].getName)
-      .appName("GeoTemplate")
+      .appName("GeoClipper")
       .getOrCreate()
     val appId = spark.sparkContext.getConf.get("spark.app.id")
-    val comma = System.getProperty("sun.java.command")
-    logger.info(s"${appId}|${comma}")
+    val command = System.getProperty("sun.java.command")
+    logger.info(s"${appId}|${command}")
     GeoSparkSQLRegistrator.registerAll(spark)
     val model = new PrecisionModel(params.precision())
     implicit val geofactory = new GeometryFactory(model)
@@ -35,11 +38,13 @@ object Clipper{
     logger.info("Reading data...")
     val partitions = params.partitions()
     val input = params.input()
-    val polysRDD = read(input)
-    polysRDD.spatialPartitioning(GridType.QUADTREE, params.partitions())
-    val regionWKT = "Polygon ((-150066.4127 -605327.0480, 310002.8938 -605327.0480, 310002.8938 -77440.1680, -150066.4127 -77440.1680, -150066.4127 -605327.0480))"
+    val polysRDD = read(input, delimiter)
+    polysRDD.spatialPartitioning(GridType.QUADTREE, partitions)
+
+    val regionWKT = Source.fromFile(params.region()).getLines.next()
+    logger.info(s"Region: $regionWKT")
     val reader = new WKTReader(geofactory)
-    val region = reader.read(regionWKT).asInstanceOf[Polygon]
+    val region = reader.read(regionWKT)
     logger.info("Reading data... Done!")
 
     logger.info("Clipping...")
@@ -48,53 +53,59 @@ object Clipper{
     val result = RangeQuery.SpatialRangeQuery(polysRDD, region,
       considerBoundaryIntersection, usingIndex)
     logger.info("Clipping... Done!")
-
     
-    logger.info("Saving to file...")
-    val f = new java.io.PrintWriter(params.output())
-    val data = result.rdd.map{ poly =>
-      val wkt = poly.toText
-
-      s"$wkt\n"
-    }.collect
-    f.write(data.mkString(""))
-    f.close()
-    logger.info(s"Saving to file... Done! (${data.size} polygons).")
+    logger.info("Saving data...")
+    val data = result.rdd.map{ geom =>
+      val wkt = geom.toText
+      val data = geom.getUserData
+      s"$wkt$delimiter$data"
+    }
+    if(params.hdfs()){
+      data.saveAsTextFile(params.output())
+    } else {
+      val f = new java.io.PrintWriter(params.output())
+      f.write(data.collect.mkString("\n"))
+      f.close()
+    }
+    logger.info(s"Saving to ${params.output()}... Done! (${data.count} polygons).")
     
     logger.info("Closing session...")
     spark.close()
     logger.info("Closing session... Done!")
   }
 
-  def read(input: String)
+  def read(input: String, delimiter: String)
     (implicit spark: SparkSession, geofactory: GeometryFactory, logger: Logger)
-      : SpatialRDD[Polygon] = {
+      : SpatialRDD[Geometry] = {
     import spark.implicits._
-    val polys = spark.read
-      .option("header", true).option("delimiter", "\t")
-      .csv(input).rdd.mapPartitions{ it =>
+    val geoms = spark.read
+      .text(input).rdd.mapPartitions{ it =>
         val reader = new WKTReader(geofactory)
         it.map{ row =>
-          val wkt = row.getString(0)
-          val lab = row.getString(1)
-          val poly = reader.read(wkt).asInstanceOf[Polygon]
+          val fields = row.getString(0).split(delimiter)
+          val wkt = fields(0)
+          val lab = fields.tail.mkString(delimiter)
+          val poly = reader.read(wkt)
           poly.setUserData(lab)
           poly
         }
       }
-    logger.info(s"Input polygons: ${polys.count()}")
-    val raw = new SpatialRDD[Polygon]
-    raw.setRawSpatialRDD(polys)
+    logger.info(s"Input geometries: ${geoms.count()}")
+    val raw = new SpatialRDD[Geometry]
+    raw.setRawSpatialRDD(geoms)
     raw.analyze()
     raw    
   }
 }
 
 class ClipperConf(args: Seq[String]) extends ScallopConf(args) {
-  val input: ScallopOption[String]  = opt[String]  (required = true)
-  val output: ScallopOption[String]  = opt[String]  (required = true)
-  val partitions: ScallopOption[Int]     = opt[Int] (default = Some(960))
-  val precision: ScallopOption[Double]     = opt[Double] (default = Some(1000.0))
+  val input: ScallopOption[String]     = opt[String]  (required = true)
+  val output: ScallopOption[String]    = opt[String]  (default = Some("/tmp/edgesClip.wkt"))
+  val partitions: ScallopOption[Int]   = opt[Int]     (default = Some(1024))
+  val precision: ScallopOption[Double] = opt[Double]  (default = Some(1000.0))
+  val delimiter: ScallopOption[String] = opt[String]  (default = Some("\t"))
+  val region: ScallopOption[String]    = opt[String]  (default = Some("/tmp/region.wkt"))
+  val hdfs: ScallopOption[Boolean]     = opt[Boolean] (default = Some(false))
 
   verify()
 }
