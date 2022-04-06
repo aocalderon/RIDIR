e6cca41e3a11a42ad9f0aba142f91495df5d2221
acalderon <acald013@ucr.edu>
2022-04-06 08:49:09 -0700

    Sync from DBLab...

---
+++ Data/gadm/gadm.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+setwd("~/RIDIR/Data/gadm/")
+
+data0 = enframe(read_lines("GADM_v01.txt"), value="line")
+data1 = data0 %>%
+  filter(str_detect(line, 'TIME')) 
+
+fields1 = c("ts","start","appId","time","tag","stage","data")
+fields2 = c("partitions", "dataset", "tolerance", "run")
+data2 = data1 %>% 
+  separate(sep = "\\|", col = "line", into = fields1, extra = "drop") %>%
+  separate(sep = "_"  , col = "data", into = fields2, extra = "drop") %>%
+  filter(stage == "layer1" | stage == "layer2" | stage == "overlay") %>%
+  select(time, stage, partitions, appId) %>%
+  mutate(time = as.numeric(time) / 1000.0) %>%
+  mutate(partitions = fct_relevel(partitions, "1000", "2000", "3000", "4000", "5000", "6000", "7000", "8000", "9000", "10000", "11000", "12000", "13000", "14000", "15000")) %>%
+  add_column(size = "gadm")
+
+data3 = data2 %>%
+  group_by(partitions, stage, size) %>% summarise(time = mean(time)) 
+
+write_tsv(data3, "gadm.tsv")
+
+p = ggplot(data3, aes(x = partitions, y = time, fill = stage)) + 
+  geom_col(width = 0.7, position="dodge") + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  labs(x="Number of partitions", y="Time [s]", title=paste0("Performance GADM dataset (65M edges) for SDCEL computation"))
+plot(p)
+
+ggsave(paste0("gadm.pdf"), width = 8, height = 5)
