76f69977e8dec6410aea0ed00dbaabdf216d37dc
acalderon <acald013@ucr.edu>
2022-04-14 15:34:40 -0700

    Sync from dblab...

---
+++ Data/OR/OR_Overlay_Test.R
@@ -0,0 +1,49 @@
+library(tidyverse)
+setwd("~/RIDIR/Data/OR/")
+
+data0a = enframe(read_lines("OR_Overlay_Test_v01.txt"), value="line")
+data0b = enframe(read_lines("OR_Overlay_Test_v02.txt"), value="line")
+data0  = bind_rows(data0a, data0b)
+
+data1 = data0 %>% filter(str_detect(line, 'TIME')) 
+
+fields1 = c("ts","start","appId","time","tag","stage","data")
+fields2 = c("dataset", "tolerance", "overlay_option", "overlay_level", "partitions", "run")
+data2 = data1 %>% 
+  separate(sep = "\\|", col = "line", into = fields1, extra = "drop") %>%
+  separate(sep = "_"  , col = "data", into = fields2, extra = "drop") %>%
+  filter(stage == "overlay" | stage == "overlayMaster" | stage == "overlayByLevel") %>%
+  select(time, stage, overlay_option, overlay_level, appId) %>%
+  mutate(time = as.numeric(time) / 1000.0, overlay_level = as.numeric(overlay_level)) 
+
+method_labels = c("By Label", 
+                  "At Master/Root", 
+                  "At Level [4]", 
+                  "At Level [5]", 
+                  "At Level [6]", 
+                  "At Level [7]", 
+                  "At Level [8]", 
+                  "At Level [9]", 
+                  "At Level [10]", 
+                  "At Level [11]", 
+                  "At Level [12]")
+data3 = data2 %>%
+  group_by(overlay_option, overlay_level, stage) %>% summarise(time = mean(time)) %>%
+  mutate(method = as.factor(if(overlay_option == 0){ "By Label" } 
+                  else { 
+                    if(overlay_option == 1){ "At Master/Root" } 
+                    else { paste0("At Level [", overlay_level, "]") } 
+                  }
+         )) %>%
+  mutate(method = fct_relevel(method, method_labels)) %>%
+  select(method, time, overlay_option, overlay_level, stage)
+
+write_tsv(data3, "OR.tsv") 
+
+p = ggplot(data3, aes(x = method, y = time)) + 
+  geom_col(width = 0.7, position="dodge") + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  labs(x="Method for overlay", y="Time [s]", title=paste0("Performance Oregon census tracts dataset (3.5M edges) for SDCEL computation"))
+plot(p)
+
+ggsave(paste0("OR.pdf"), width = 8, height = 5)
