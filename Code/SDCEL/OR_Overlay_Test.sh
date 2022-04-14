8fc72c66c449b9d85666fb9a50476fdb4f7f114a
Andres Calderon <acald013@ucr.edu>
2022-04-14 13:10:31 -0700

    Collecting more results...

--- Code/SDCEL/OR_Overlay_Test.sh
+++ Code/SDCEL/OR_Overlay_Test.sh
@@ -3,11 +3,17 @@
 DATASET="OR"
 PARTITIONS=464
 
-./Perf -d Census/S/$DATASET -t 1e-3 -p $PARTITIONS -m yarn -o 0 -n 5
+#./Perf -d Census/S/$DATASET -t 1e-3 -p $PARTITIONS -m yarn -o 0 -n 5
 
-./Perf -d Census/S/$DATASET -t 1e-3 -p  -m yarn -o 1 -n 5
+./Perf -d Census/S/$DATASET -t 1e-3 -p $PARTITIONS -m yarn -o 1 -n 5
 
-./Perf -d Census/S/$DATASET -t 1e-3 -p $PARTITIONS -m yarn -o 2 -e 4 -n 5
-./Perf -d Census/S/$DATASET -t 1e-3 -p $PARTITIONS -m yarn -o 2 -e 6 -n 5
-./Perf -d Census/S/$DATASET -t 1e-3 -p $PARTITIONS -m yarn -o 2 -e 8 -n 5
-./Perf -d Census/S/$DATASET -t 1e-3 -p $PARTITIONS -m yarn -o 2 -e 10 -n 5
+#./Perf -d Census/S/$DATASET -t 1e-3 -p $PARTITIONS -m yarn -o 2 -e 4 -n 5
+#./Perf -d Census/S/$DATASET -t 1e-3 -p $PARTITIONS -m yarn -o 2 -e 6 -n 5
+#./Perf -d Census/S/$DATASET -t 1e-3 -p $PARTITIONS -m yarn -o 2 -e 8 -n 5
+#./Perf -d Census/S/$DATASET -t 1e-3 -p $PARTITIONS -m yarn -o 2 -e 10 -n 5
+
+./Perf -d Census/S/$DATASET -t 1e-3 -p $PARTITIONS -m yarn -o 2 -e 5 -n 5
+./Perf -d Census/S/$DATASET -t 1e-3 -p $PARTITIONS -m yarn -o 2 -e 7 -n 5
+./Perf -d Census/S/$DATASET -t 1e-3 -p $PARTITIONS -m yarn -o 2 -e 9 -n 5
+./Perf -d Census/S/$DATASET -t 1e-3 -p $PARTITIONS -m yarn -o 2 -e 11 -n 5
+./Perf -d Census/S/$DATASET -t 1e-3 -p $PARTITIONS -m yarn -o 2 -e 12 -n 5
