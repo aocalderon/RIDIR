6631a055980b40f43d43d53ef3a0feca74368195
Andres Calderon <acald013@ucr.edu>
2022-04-19 14:19:01 -0700

    Adding data for FL...

--- Code/SDCEL/Overlay_Tester_Runner.sh
+++ Code/SDCEL/Overlay_Tester_Runner.sh
@@ -6,5 +6,6 @@
 #./Overlay_Tester.sh -d TN -p 693
 #./Overlay_Tester.sh -d GA -p 653
 
-./Overlay_Tester.sh -d VA -p 655
-./Overlay_Tester.sh -d PA -p 585
+#./Overlay_Tester.sh -d VA -p 655
+#./Overlay_Tester.sh -d PA -p 585
+./Overlay_Tester.sh -d FL -p 598
