699ebd0cea1931b96223aae8536e6bc279a5ef7a
Andres Calderon <acald013@ucr.edu>
2022-04-19 09:40:15 -0700

    Collecting last results and running another couple...

--- Code/SDCEL/State_Overlay_Test_Runner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./State_Overlay_Test.sh -d TX -p 1417
-./State_Overlay_Test.sh -d CA -p 1432
-./State_Overlay_Test.sh -d NC -p 764
-./State_Overlay_Test.sh -d TN -p 693
-./State_Overlay_Test.sh -d GA -p 653
